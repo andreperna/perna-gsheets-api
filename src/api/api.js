85351a195005274f342dd8a463566f12b0a253a6
andreperna <andreperna@gmail.com>
2023-05-01 00:55:27 -0300

    add cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors"
 import { swaggerMiddleware } from "../swagger/swaggerMiddleware.js";
 import { router as routesIndex } from "./routes/routes.js";
 // import swaggerDoc from "../swagger/swagger-output.json" assert { type: "json" };
 
 
 export const api = express();
+
+api.use(cors())
 
 api.set ("json spaces", 2)
 api.use(express.json())
